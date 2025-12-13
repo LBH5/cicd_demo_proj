a9cc0ba2dd3c2f927a030ec87d1eeda35c2c4bb8
Naime5_Br <153781554+LBH5@users.noreply.github.com>
2025-12-13 16:55:15 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="main">
+    <h1>Github Actions!</h1>
+    <p>
+      For Beginners
+    </p>
+  </section>
+</template>
+
+<style scoped>
+
+  .main {
+    background:linear-gradient(to right, #65ff1e,#06ff06);
+    background-size: cover;
+    width:100%;
+    height: 100dvh;
+    display: grid;
+    place-content: center;
+    color:white
+  }
+
+  h1,p{
+    font-family: Quicksand;
+    font-weight: 800;
+  }
+</style>
